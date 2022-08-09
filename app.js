8e5111ee8cf3fe31c16f2cb622e0b9c32473fdc8
manu3006 <110452229+manu3006@users.noreply.github.com>
2022-08-09 20:52:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,3 +3,42 @@ $('.banner-video a').fancybox({
     caption : function( instance, item ) {
     }
   });
+  // end
+
+
+  
+//   var a = 0;
+// $(window).scroll(function () {
+//     var oTop = $(".counter-number").offset().top - window.innerHeight;
+//     if (a == 0 && $(window).scrollTop() > oTop) {
+//         $(".counter").each(function () {
+//             var $this = $(this),
+//                 countTo = $this.attr("data-number");
+//             $({
+//                 countNum: $this.text()
+//             }).animate(
+//                 {
+//                     countNum: countTo
+//                 },
+
+//                 {
+//                     duration: 850,
+//                     easing: "swing",
+//                     step: function () {
+//                         //$this.text(Math.ceil(this.countNum));
+//                         $this.text(
+//                             Math.ceil(this.countNum).toLocaleString("en")
+//                         );
+//                     },
+//                     complete: function () {
+//                         $this.text(
+//                             Math.ceil(this.countNum).toLocaleString("en")
+//                         );
+//                         //alert('finished');
+//                     }
+//                 }
+//             );
+//         });
+//         a = 1;
+//     }
+// });
